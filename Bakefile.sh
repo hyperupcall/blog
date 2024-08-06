440972921192274e5bff1c60e080d4f9f7e32ab2
Edwin Kofler <edwin@kofler.dev>
2024-08-06 01:40:16 -0700

    Replace Hugo blog with redirects to `edwinkofler.com`
    My blog content is now part of my main website. Hugo is being replaced
    with a custom solution called Rho. For now, Rho is mostly contained in a
    single rho.js[1] file, which is in my main website repository.
    
    [1](https://github.com/fox-projects/edwinkofler.com/blob/aa9957121010f9408ecfb27c46e0d2a5e1954a1a/rho.js)

--- Bakefile.sh
+++ Bakefile.sh
@@ -1,9 +1,53 @@
 # shellcheck shell=bash
 
-task.dev() {
-	hugo serve
+task.prettier() {
+	./node_modules/.bin/prettier --write .
 }
 
-task.build() {
-	hugo
+task.generate() {
+	local -a dirs=(
+		''
+		posts/new-blog
+		posts/render-latex-with-katex-in-hugo-blog
+		posts/fibonacci-equation-using-pascals-triangle-part-1
+		posts/fibonacci-equation-using-pascals-triangle-part-2
+		posts/creating-website-for-robotics-club
+		posts/front-end-web-dev-a-years-reflection
+		posts/fiddling-with-ubuntu-server-images
+		posts/fixing-my-internal-network
+		posts/terminal-automation-with-expect
+		posts/semantic-hotkeys
+		posts/umpire-method
+	)
+
+	for dir in "${dirs[@]}"; do
+		if [ -z "$dir" ]; then
+			route='posts'
+		else
+			route=$dir
+		fi
+
+		mkdir -p "./$dir"
+		cat > "./$dir/index.html" <<EOF
+<!doctype html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		<link rel="canonical" href="https://edwinkofler.com/$route" />
+		<meta http-equiv="refresh" content="0; url=https://edwinkofler.com/$route" />
+		<title>Redirecting...</title>
+		<script>
+			window.location.href = 'https://edwinkofler.com/$route/'
+		</script>
+	</head>
+	<body>
+		<p>
+			If you are not redirected automatically, click
+			<a href="https://edwinkofler.com/$route/">here</a>
+		</p>
+	</body>
+</html>
+EOF
+	done
 }
--- archetypes/default.md
+++
@@ -1,9 +0,0 @@
-+++
-title = "{{ replace .Name "-" " " | title }}"
-slug = "{{ urlize .Name }}"
-author = "Edwin Kofler"
-date = {{ .Date }}
-categories = []
-tags = []
-draft = true
-+++
